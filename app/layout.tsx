b439a558d7048654d7b6a91c2a26c8fbc7f4f76f
Starboy756 <taseentariq5@gmail.com>
2025-12-04 19:51:12 +0500

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,6 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Jobpilot | Your Very Own Job Buddy",
   description: "Jobpilot - Find your dream job today.",
-  icons: {
-    icon: "/favicon.svg",
-  },
 };
 
 export default function RootLayout({
